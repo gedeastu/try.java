65f1495ceebc5348e6042a393d6aecf152b0879e
Hollyyear <gede.astu@gmail.com>
2022-10-04 16:42:55 +0700

    Already done

---
+++ src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        char satu = '1';
+        int number = 1;
+        System.out.println(satu + number);
+    }
+}
